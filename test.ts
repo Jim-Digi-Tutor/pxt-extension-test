89272b4bcce0cf6fd83fe564dc716f0982503142
Jim Taylor <157226261+Jim-Digi-Tutor@users.noreply.github.com>
2025-02-06 18:18:14 +0000

    Update test.ts

--- test.ts
+++ test.ts
@@ -1,1 +1,15 @@
-// tests go here; this will not be compiled when this package is used as an extension.
+
+/**
+ * Functions are mapped to blocks using various macros
+ * in comments starting with %. The most important macro
+ * is "block", and it specifies that a block should be
+ * generated for an **exported** function.
+ */
+
+//% color="#AA278D" weight=100
+namespace jimTest {
+    //% block="My Test Function $newNum"
+    export function testFunction(newNum:number) : number{
+        return newNum * 2;
+    }
+}
